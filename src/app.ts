9a08d6105676770d3a524150aec5eadbac00fd31
vizcoder <109105171+vizcoder@users.noreply.github.com>
2023-02-14 11:01:40 +0530

    Add files via upload

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import "express-async-errors";
+
+import express, { json } from "express";
+import helmet from "helmet";
+
+const app = express();
+app.use(json());
+app.use(helmet());
+
+app.get("/", (_, res) => {
+  res.json({
+    msg: "Hello World",
+  });
+});
+
+app.use((_, res, _2) => {
+  res.status(404).json({ error: "NOT FOUND" });
+});
+
+export { app };
